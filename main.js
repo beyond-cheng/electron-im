7b52eda8a04ffd23b517cbcb8634d042054f81d8
lidongdong <626462591@qq.com>
2021-11-30 20:57:06 +0800

    add react

--- main.js
+++ main.js
@@ -2,19 +2,18 @@ const electron = require('electron')
 const {app, BrowserWindow} = electron;
 const path = require('path')
 
+
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 1200
     });
 
-    mainWindow.loadFile('./page/index.html')
+    mainWindow.loadURL('http://127.0.0.1:3000')
 }
 
 app.whenReady().then(() => {
     createWindow();
-
-
 })
 
 app.on('window-all-closed', () => {
